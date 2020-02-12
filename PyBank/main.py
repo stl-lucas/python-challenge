5291660eae86057881f4b620199f180b8a36adc7
Tim <tllucas1s@gmail.com>
2020-02-12 17:15:17 -0600

    Added csv files for both challenges and added variables and final print summary placeholders in both python files.

---
+++ PyBank/main.py
@@ -0,0 +1,22 @@
+#Variable Assignments
+total_months = 86
+total_amount = 38382578
+average_change = -2315.12
+increase_date = "Feb-2012"
+increase_amount = 1926159
+decrease_date = "Sep-2013"
+decrease_amount = -2196167
+
+#CSV Import and Analysis
+
+
+#Final Print Statement
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_amount}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {increase_date} ({increase_amount})")
+print(f"Greatest Decrease in Profits: {decrease_date} ({decrease_amount})")
+
+#Export text file with results
---
+++ PyPoll/main.py
@@ -0,0 +1,26 @@
+#Variable Assignments
+total_votes = 3521001
+candidate_name = "Khan"
+vote_perc = 63.000
+vote_count = 2218231
+winner = "Khan"
+
+
+#CSV Import and Analysis
+
+
+#Final Print Statement
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------------")
+
+#for each candidate in list
+print(f"{candidate_name}: {vote_perc}% ({vote_count})")
+#end for
+
+print("----------------------------")
+print(f"Winner: {winner}")
+print("----------------------------")
+
+#Export text file with results
