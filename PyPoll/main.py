74ee6882766f666af7f82cee2b3313c7ab13c2a4
Tim <tllucas1s@gmail.com>
2020-02-13 15:53:55 -0600

    Finished calculations, print statements, and .txt file creation using a set dictionary. Still need to process .csv to build dictionary.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,15 @@
-#Variable Assignments
-total_votes = 3521001
-candidate_name = "Khan"
-vote_perc = 63.000
-vote_count = 2218231
-winner = "Khan"
+import operator
 
+#Variable Assignments
+total_votes = 0
+votes = {'Khan': 2218231, 'Correy': 704200, 'Li': 492940, 'O\'Tooley': 105630 }
 
 #CSV Import and Analysis
+winner = max(votes, key=votes.get)
 
+#Calculations
+for k, v in votes.items():
+    total_votes = total_votes + v
 
 #Final Print Statement
 print("Election Results")
@@ -16,7 +18,9 @@ print(f"Total Votes: {total_votes}")
 print("----------------------------")
 
 #for each candidate in list
-print(f"{candidate_name}: {vote_perc}% ({vote_count})")
+for k, v in votes.items():
+    percent = format(v / total_votes * 100, '.3f')
+    print("{0}: {2}% ({1})".format(k, v, percent))
 #end for
 
 print("----------------------------")
@@ -24,3 +28,17 @@ print(f"Winner: {winner}")
 print("----------------------------")
 
 #Export text file with results
+file_1 = open("PyPollResult.txt","w") 
+file_1.write("Election Results\n")
+file_1.write("----------------------------\n")
+file_1.write(f"Total Votes: {total_votes}\n")
+file_1.write("----------------------------\n")
+
+for k, v in votes.items():
+    percent = format(v / total_votes * 100, '.3f')
+    file_1.write("{0}: {2}% ({1})\n".format(k, v, percent))
+
+file_1.write("----------------------------\n")
+file_1.write(f"Winner: {winner}\n")
+file_1.write("----------------------------\n")
+file_1.close() 
