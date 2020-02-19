036c6644829beb2032c73946d9613f0d321a294c
Tim <tllucas1s@gmail.com>
2020-02-18 22:02:56 -0600

    finished final script and verified result is correcte and prtinted to a text file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,35 @@
 import csv
 
 #Variable Initialization
+records = {}
+changes = {}
 total_months = 0
 total_amount = 0
+change_amount = 0
 
 #CSV Import and Analysis
-records = {'Jan-10': 867884, 'Feb-10': 984655, 'Mar-10': 322013, 'Apr-10': -69417 }
+csvpath = "budget_data.csv"
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    for row in csvreader:
+        changes[row[0]] = int(row[1]) - change_amount
+        records[row[0]] = int(row[1])
+        change_amount = int(row[1])
 
 #Calculations
 for k, v in records.items():
     total_months = total_months + 1
     total_amount = total_amount + v
 
-increase_date = max(records, key=records.get)
-increase_amount = max(records.values())
-decrease_date = min(records, key=records.get)
-decrease_amount = min(records.values())
-average_change = format(total_amount / total_months, '.2f')
+increase_date = max(changes, key=changes.get)
+increase_amount = max(changes.values())
+
+decrease_date = min(changes, key=changes.get)
+decrease_amount = min(changes.values())
+
+average_change = format((sum(changes.values()) - list(records.values())[0]) / (total_months - 1), '.2f')
 
 #Final Print Statement
 print("Financial Analysis")
@@ -24,8 +37,8 @@ print("----------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${total_amount}")
 print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: {increase_date} ({increase_amount})")
-print(f"Greatest Decrease in Profits: {decrease_date} ({decrease_amount})")
+print(f"Greatest Increase in Profits: {increase_date} (${increase_amount})")
+print(f"Greatest Decrease in Profits: {decrease_date} (${decrease_amount})")
 
 #Export text file with results
 file_1 = open("PyBankResult.txt","w") 
@@ -34,6 +47,6 @@ file_1.write("----------------------------\n")
 file_1.write(f"Total Months: {total_months}\n")
 file_1.write(f"Total: ${total_amount}\n")
 file_1.write(f"Average Change: ${average_change}\n")
-file_1.write(f"Greatest Increase in Profits: {increase_date} ({increase_amount})\n")
-file_1.write(f"Greatest Decrease in Profits: {decrease_date} ({decrease_amount})\n")
+file_1.write(f"Greatest Increase in Profits: {increase_date} (${increase_amount})\n")
+file_1.write(f"Greatest Decrease in Profits: {decrease_date} (${decrease_amount})\n")
 file_1.close() 
